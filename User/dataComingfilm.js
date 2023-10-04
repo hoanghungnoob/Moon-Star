7dc5de7c6652dc959975e7f47d79697032670276
Admin <admin@vinasupport.com>
2023-10-04 23:47:23 +0700

    add comingSoonfilm

--- User/dataComingfilm.js
+++ User/dataComingfilm.js
@@ -1,72 +1,50 @@
 // Tạo một hàm để lấy dữ liệu từ tệp JSON
-function getComingFilm(callback) {
-    fetch('../database/data.json') // Đường dẫn đến tệp JSON
-      .then(response => response.json())
-      .then(data => {
-        // Lấy mảng "comingSon" từ dữ liệu JSON
-        const comingSoon = data.comingSoon;
-  
-        // Gọi hàm callback và truyền giá trị "comingSon" vào nó
-        callback(comingSoon);
-  
-        // Gọi hàm comingProduct để hiển thị danh sách phim
-        comingProduct(comingSoon);
-      })
-      .catch(error => {
-        console.error('Lỗi khi đọc tệp JSON:', error);
-      });
-  }
-  
-  // Hàm để hiển thị danh sách phim lên màn hình
-  function comingProduct(comingSoon) {
-    const itemContainer = document.getElementById("item");
-  
-    comingSoon.forEach(item => {
-      const filmCard = document.createElement("div");
-      filmCard.classList.add("film-card");
-  
-      const image = document.createElement("img");
-      image.src = item.image;
-      image.classList.add("film-image");
-  
-      const cardBody = document.createElement("div");
-      cardBody.classList.add("card-body");
-  
-      const title = document.createElement("h5");
-      title.textContent = item.name;
-  
-      const details = document.createElement("p");
-      details.textContent = `${item.time} | ${item.date}`;
-  
-      const buttonsContainer = document.createElement("div");
-      buttonsContainer.classList.add("buttons-container");
-  
-    //   const detailButton = document.createElement("input");
-    //   detailButton.type = "button";
-    //   detailButton.value = "Detail";
-    //   detailButton.addEventListener("click", function() {
-    //     // Xử lý sự kiện khi nhấn nút "Detail"
-    //     // Đặt logic xử lý ở đây
-    //   });
-  
-    //   const bookingButton = document.createElement("input");
-    //   bookingButton.type = "button";
-    //   bookingButton.value = "Booking Now";
-  
-    //   buttonsContainer.appendChild(detailButton);
-    //   buttonsContainer.appendChild(bookingButton);
-  
-      cardBody.appendChild(title);
-      cardBody.appendChild(details);
-      cardBody.appendChild(buttonsContainer);
-  
-      filmCard.appendChild(image);
-      filmCard.appendChild(cardBody);
-  
-      itemContainer.appendChild(filmCard);
+function getListFilm(callback) {
+  fetch('../database/data.json') // Đường dẫn đến tệp JSON
+    .then(response => response.json())
+    .then(data => {
+      // Lấy mảng "listfilm" từ dữ liệu JSON
+      const comingSoon = data.comingSoon;
+      data.comingSoon
+
+      // Gọi hàm callback và truyền giá trị "listfilm" vào nó
+      callback(comingSoon);
+    })
+    .catch(error => {
+      console.error('Lỗi khi đọc tệp JSON:', error);
     });
-  }
+}
+
+// Hàm để hiển thị danh sách phim lên màn hình
+function listProduct(comingSoon) {
+  const itemContainer = document.getElementById("item");
   
-  getComingFilm(comingProduct);
-  
-  
+  comingSoon.forEach(item => {
+    const filmCard = document.createElement("div");
+    filmCard.classList.add("film-card");
+    
+    const image = document.createElement("img");
+    image.src = item.image;
+    image.classList.add("film-image");
+    
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    
+    const title = document.createElement("h5");
+    title.textContent = item.name;
+    
+    const details = document.createElement("p");
+    details.textContent = `${item.time} | ${item.date}`;
+    
+    cardBody.appendChild(title);
+    cardBody.appendChild(details);
+    
+    filmCard.appendChild(image);
+    filmCard.appendChild(cardBody);
+    
+    itemContainer.appendChild(filmCard);
+  });
+}
+
+// Gọi hàm getListFilm và truyền hàm listProduct làm callback
+getListFilm(listProduct);

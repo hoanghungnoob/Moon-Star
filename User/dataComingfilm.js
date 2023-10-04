6001f10a9c00c76a4824d1bd32d8f20b643fb0ae
Hung2201 <142329825+Hung2201@users.noreply.github.com>
2023-10-04 23:54:58 +0700

    Homepage (#9)
    * first commit add register
    
    * thay doi lan 2
    
    * add register
    
    * addcommingsoonfilm
    
    * add comingSoonfilm
    
    ---------
    
    Co-authored-by: Admin <admin@vinasupport.com>

---
+++ User/dataComingfilm.js
@@ -0,0 +1,50 @@
+// Tạo một hàm để lấy dữ liệu từ tệp JSON
+function getListFilm(callback) {
+  fetch('../database/data.json') // Đường dẫn đến tệp JSON
+    .then(response => response.json())
+    .then(data => {
+      // Lấy mảng "listfilm" từ dữ liệu JSON
+      const comingSoon = data.comingSoon;
+      data.comingSoon
+
+      // Gọi hàm callback và truyền giá trị "listfilm" vào nó
+      callback(comingSoon);
+    })
+    .catch(error => {
+      console.error('Lỗi khi đọc tệp JSON:', error);
+    });
+}
+
+// Hàm để hiển thị danh sách phim lên màn hình
+function listProduct(comingSoon) {
+  const itemContainer = document.getElementById("item");
+  
+  comingSoon.forEach(item => {
+    const filmCard = document.createElement("div");
+    filmCard.classList.add("film-card");
+    
+    const image = document.createElement("img");
+    image.src = item.image;
+    image.classList.add("film-image");
+    
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    
+    const title = document.createElement("h5");
+    title.textContent = item.name;
+    
+    const details = document.createElement("p");
+    details.textContent = `${item.time} | ${item.date}`;
+    
+    cardBody.appendChild(title);
+    cardBody.appendChild(details);
+    
+    filmCard.appendChild(image);
+    filmCard.appendChild(cardBody);
+    
+    itemContainer.appendChild(filmCard);
+  });
+}
+
+// Gọi hàm getListFilm và truyền hàm listProduct làm callback
+getListFilm(listProduct);

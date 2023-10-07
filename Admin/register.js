9aaabcc051459c70713cb6b2219c13152ad33866
hoang hung <hoanghung20030@gmail.com>
2023-10-07 09:29:20 +0700

    add file admin change file register change file homepage2 userlogin data.json

--- Admin/register.js
+++ Admin/register.js
@@ -14,7 +14,7 @@ function register() {
   let password = document.getElementById("password").value;
   let repassword = document.getElementById("repassword").value;
   let id =
-    RandomHexString(4) + "-" + RandomHexString(4) + "-" + RandomHexString(4);
+    RandomHexString(4) + "-" + RandomHexString(4);
 
   console.log(id);
   // Create an object with the data
@@ -27,11 +27,13 @@ function register() {
     password == repassword
   ) {
     let userData = {
+      id,
       name: fname,
+      lastname: lname, 
       pnumber: pnumber,
       email: email,
       password: password,
-      id,
+      gender: ""
     };
     console.log(userData);
     // Make a POST request to http://localhost:3000/user

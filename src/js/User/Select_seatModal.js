37f4f955a0cbaebe2c84586e3123ac977c611858
Admin <admin@vinasupport.com>
2023-10-23 20:34:29 +0700

    Hung day dong

---
+++ src/js/User/Select_seatModal.js
@@ -0,0 +1,56 @@
+let bookingButton = document.getElementById("booking");
+
+let modal = document.getElementById("select_seat_modal");
+
+bookingButton.addEventListener("click", () => {
+    modal.style.display = "block"; 
+});
+
+document.getElementById("booking").addEventListener("click", (e) => {
+    if (e.target === this){
+        
+        let modal = document.getElementById("select_seat_modal");
+        modal.style.display="none";
+    }
+});
+
+
+
+function changeSeatColor(seatId) {
+    let seat = document.getElementById(seatId);
+    let label = seat.nextElementSibling;
+    let cost = 0;
+
+    if (seat.checked) {
+        label.style.backgroundColor = "#ff0000"; // Màu ghế đã chọn
+
+    } else {
+        label.style.backgroundColor = "#ccc"; // Màu ghế mặc định
+    }
+
+    let check = document.getElementsByName('chair'); // Đổi 'checkbox' thành 'chair'
+    let length = check.length;
+    let selectedSeats = [];
+
+    for (let i = 0; i < length; i++) {
+        if (check[i].checked) {
+            selectedSeats.push(check[i].nextElementSibling.textContent);
+        }
+    }
+
+    cost = selectedSeats.length * 90000; // Tính giá vé dựa trên số ghế đã chọn
+    localStorage.setItem("cost", JSON.stringify(cost));
+    document.getElementById("result").innerHTML = "Bạn đã mua vé với số tiền là: " + cost + " VND";
+    document.getElementById("selected-seats").textContent = "Ghế đã chọn: " + selectedSeats.join(", ");
+}
+
+const backButton = document.getElementById("back-button");
+
+backButton.addEventListener("click", () => {   
+    closeModal(); 
+});
+
+function closeModal() { 
+    const modal = document.getElementById("select_seat_modal");
+    modal.style.display = "none";
+}
--- src/js/User/login.js
+++ src/js/User/login.js
@@ -1,65 +1,89 @@
 document.getElementById("loginBtn").addEventListener("click", openLoginModal);
 
-        function openLoginModal() {
-            let loginModal = document.getElementById("loginModal");
-            loginModal.style.display = "block";
-        }
+function openLoginModal() {
+    let loginModal = document.getElementById("loginModal");
+    loginModal.style.display = "block";
+}
 
-        document.getElementById("loginModal").addEventListener("click", function (e) {
-            if (e.target === this) {
-                let loginModal = document.getElementById("loginModal");
-                loginModal.style.display = "none";
-            }
-        });
+document.getElementById("loginModal").addEventListener("click", function (e) {
+    if (e.target === this) {
         let loginModal = document.getElementById("loginModal");
-        let showRegisterModalLink = document.getElementById("showRegisterModal");
-        let registerModal = document.getElementById("registerModal");
+        loginModal.style.display = "none";
+    }
+});
+let loginModal = document.getElementById("loginModal");
+let showRegisterModalLink = document.getElementById("showRegisterModal");
+let registerModal = document.getElementById("registerModal");
 
-    // Lắng nghe sự kiện khi nhấp vào liên kết "Login" trong form đăng ký
-        showRegisterModalLink.addEventListener("click", function (e) {
-        e.preventDefault(); // Ngăn chặn hành vi mặc định của liên kết
+// Lắng nghe sự kiện khi nhấp vào liên kết "Login" trong form đăng ký
+showRegisterModalLink.addEventListener("click", function (e) {
+    e.preventDefault(); // Ngăn chặn hành vi mặc định của liên kết
 
-        registerModal.style.display = "block"; // Hiển thị modal đăng ký
-        loginModal.style.display = "none"; // Ẩn modal đăng ký
-    });
-        
+    registerModal.style.display = "block"; // Hiển thị modal đăng ký
+    loginModal.style.display = "none"; // Ẩn modal đăng nhập
+});
 
-        // Xử lý đăng nhập khi người dùng submit form
-        document.getElementById("loginForm").addEventListener("submit", function (event) {
-            event.preventDefault();
-            // Lấy giá trị nhập từ người dùng
-            const emailInput = document.getElementById("Email").value;
-            const passwordInput = document.getElementById("password").value;
 
-            fetch("../../../../../database/data.json")
-                .then(response => response.json())
-                .then(data => {
-                    const user = data.user;
-                    const admin = data.admin;
-                    const loggedInUser = user.find(user => user.email === emailInput && user.password === passwordInput);
-                    const loggedInAdmin = admin.find(admin => admin.email === emailInput && admin.password === passwordInput);
 
-                    if (loggedInUser) {
-                        window.location.href = "homepages2.html";
-
-                        
-                         localStorage.setItem("user_token_id", JSON.stringify(loggedInUser.id));
-                    } 
-                    else if(loggedInAdmin){
-              
-                         localStorage.setItem("admin_token_id", JSON.stringify(loggedInAdmin.id));
-
-                        window.location.href = "../Admin/admin.html"
-                    }
-
-                    else {
-                        alert("Unsuccessful");
-                    }
-                })
-                .catch(error => {
-                    alert("Lỗi khi tải dữ liệu người dùng:", error);
-                });
-            // Đóng modal sau khi đăng nhập thành công
-            let loginModal = document.getElementById("loginModal");
-            loginModal.style.display = "none";
-        });
+// Kiểm tra trạng thái đăng nhập khi trang chủ được tải lại
+document.addEventListener("DOMContentLoaded", function () {
+    const userToken = localStorage.getItem("user_token_id");
+    if (userToken) {
+      // Người dùng đã đăng nhập, ẩn nút đăng nhập và đăng ký, hiển thị hình profile
+      document.getElementById("loginBtn").style.display = "none";
+      document.getElementById("registerBtn").style.display = "none";
+      document.getElementById("menu_profile").style.display = "inline-block";
+    } else {
+      // Người dùng chưa đăng nhập, hiển thị nút đăng nhập và đăng ký
+      document.getElementById("loginBtn").style.display = "inline-block";
+      document.getElementById("registerBtn").style.display = "inline-block";
+      document.getElementById("menu_profile").style.display = "none";
+    }
+  });
+  
+  // Xử lý đăng nhập khi người dùng submit form
+  document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Lấy giá trị nhập từ người dùng
+    const emailInput = document.getElementById("Email").value;
+    const passwordInput = document.getElementById("password").value;
+  
+    fetch("../../../../../database/data.json")
+      .then(response => response.json())
+      .then(data => {
+        const user = data.user;
+        const admin = data.admin;
+  
+        const loggedInUser = user.find(user => user.email === emailInput && user.password === passwordInput);
+        const loggedInAdmin = admin.find(admin => admin.email === emailInput && admin.password === passwordInput);
+  
+        if (loggedInUser) {
+          // Lưu thông tin người dùng đã đăng nhập vào local storage
+          localStorage.setItem("user_token_id", JSON.stringify(loggedInUser.id));
+          
+          // Ẩn nút đăng nhập và đăng ký, hiển thị hình profile
+          document.getElementById("loginBtn").style.display = "none";
+          document.getElementById("registerBtn").style.display = "none";
+          document.getElementById("menu_profile").style.display = "inline-block";
+        } else if (loggedInAdmin) {
+          // Lưu thông tin người dùng đã đăng nhập vào local storage
+          localStorage.setItem("user_token_id", JSON.stringify(loggedInAdmin.id));
+          
+          // Chuyển hướng đến trang quản trị
+          window.location.href = "../Admin/admin.html";
+        } else {
+          alert("Unsuccessful");
+        }
+      })
+      .catch(error => {
+        alert("Lỗi khi tải dữ liệu người dùng:", error);
+      });
+  
+    // Đóng modal sau khi đăng nhập thành công
+    let loginModal = document.getElementById("loginModal");
+    loginModal.style.display = "none";
+  });
+  document.getElementById("logout").addEventListener("click", function () {
+    // Xóa thông tin người dùng đã đăng nhập khỏi local storage
+    localStorage.removeItem("user_token_id");
+  });

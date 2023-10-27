8f6b1abfbd6e857038923621d95942ca76cbe7fa
Hoàng <hoanghung20030@gmail.com>
2023-10-28 02:22:07 +0700

    update booking page

--- src/js/User/booking.js
+++ src/js/User/booking.js
@@ -1,17 +1,37 @@
-function hideIcon(){
-    const hideProfile =document.getElementById('menu_profile');
-    hideProfile.style.display='none';
-  }
-  function hideLg(){
-    const login = document.querySelector('.login');
-    login.style.display ='none';
-  }
-  function ktUser(){
-    if (localStorage.getItem('user_token_id')==null){
-      hideIcon();
+fetch('https://foregoing-messy-freckle.glitch.me/listbooking')
+  .then(res => res.json())
+  .then(data => {
+    let html = ``;
+    for (const element of data) {
+      const localName = localStorage.getItem('user_token_id');
+      const concat = `"` + element.idUser + `"`;
+      if (localName == concat) {
+        html += `
+            <tr id="detailbooking${element.idBooking}">
+              <td>${element.nameUser}</td>
+              <td>${element.phoneNumber}</td>
+              <td>${element.filmName}</td>
+              <td>${element.dateBooking}</td>
+              <td>${element.dateWatch}</td>
+              <td>${element.time}</td>
+              <td>${element.char}</td>
+              <td>${element.money}</td>
+            </tr>
+            `;
+
+      }
     }
-    else{
-      hideLg();
+    if (html != ``) {
+      document.getElementById('tableshowlistbooking').innerHTML = html;
+    } else {
+      html = '<h1>You are not booking any film. Please booking to check it again';
+      document.getElementById('returnValue').innerHTML = html;
     }
-  }
-  ktUser();
+
+
+  })
+  .catch(error => {
+    console.log('Error :', error);
+  });
+
+

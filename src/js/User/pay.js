57e4629bff38f507b499d0d3e4223f055a74e6fd
CuongLe <cuong.le@stunited.vn>
2023-11-01 15:59:06 +0700

    hotfix

--- src/js/User/pay.js
+++ src/js/User/pay.js
@@ -1,10 +1,23 @@
 const payButton = document.getElementById("buttonPay");
 payButton.addEventListener("click", () => {
-  let modalMomo = document.getElementById("payModalMomo");
-
-  let modalPay = document.getElementById("payModal");
-  modalPay.style.display="none";
-  modalMomo.style.display="block";
+  fetch("http://localhost:4002/payment", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json(); // Chuyển đổi dữ liệu JSON
+    })
+    .then((data) => {
+      window.open(data.payUrl);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
 
 

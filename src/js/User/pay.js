21a692226b5a1ab01feb45821776296b83349cb7
Hoàng <hoanghung20030@gmail.com>
2023-11-04 00:50:01 +0700

    sửa lại data

--- src/js/User/pay.js
+++ src/js/User/pay.js
@@ -1,66 +1,61 @@
-// function RandomHexString(L) {
-//   let hexstring = "";
-//   for (let i = 0; i < L; i++) {
-//     hexstring += Math.floor(Math.random() * 16).toString(16);
-//   }
-//   return hexstring;
-// }
+function RandomHexString(L) {
+  let hexstring = "";
+  for (let i = 0; i < L; i++) {
+    hexstring += Math.floor(Math.random() * 16).toString(16);
+  }
+  return hexstring;
+}
 
-//   function getCurrentDate() {
-//     var currentDate = new Date();
-//     var year = currentDate.getFullYear();
-//     var month = currentDate.getMonth() + 1; // Tháng bắt đầu từ 0
-//     var day = currentDate.getDate();
-//     // Định dạng lại ngày thành một chuỗi "YYYY-MM-DD"
-//     var formattedDate = year + "-" + padZero(month) + "-" + padZero(day);
-//     return formattedDate;
-//   }
-//   // Hàm này để đảm bảo rằng số tháng và ngày luôn có hai chữ số
-//   function padZero(value) {
-//     return value < 10 ? "0" + value : value;
-//   }
-//   // Gọi hàm để lấy ngày hiện tại
-//   let currentDate = getCurrentDate();
-//   let nameFilm = JSON.parse(localStorage.getItem('nameFilm'));
-//   let dateWatch = JSON.parse(localStorage.getItem(''))
+  function getCurrentDate() {
+    var currentDate = new Date();
+    var year = currentDate.getFullYear();
+    var month = currentDate.getMonth() + 1; // Tháng bắt đầu từ 0
+    var day = currentDate.getDate();
+    // Định dạng lại ngày thành một chuỗi "YYYY-MM-DD"
+    var formattedDate = year + "-" + padZero(month) + "-" + padZero(day);
+    return formattedDate;
+  }
+  // Hàm này để đảm bảo rằng số tháng và ngày luôn có hai chữ số
+  function padZero(value) {
+    return value < 10 ? "0" + value : value;
+  }
+  // Gọi hàm để lấy ngày hiện tại
+  let currentDate = getCurrentDate();
+  let nameFilm = JSON.parse(localStorage.getItem('nameFilm'));
+  let dateWatch = JSON.parse(localStorage.getItem(''))
+  let idUser = JSON.parse(localStorage.getItem('user_token_id'));
 
-//   let idUser = JSON.parse(localStorage.getItem('user_token_id'));
 
-//   fetch('https://mor-start.onrender.com/user')
-//     .then(res => res.json())
-//     .then(data => {
-//       for (const item of data) {     
-//         if (item.id == idUser) {
-//           nameUser = item.name;
-//           let lastname = item.lastname;
-//           let email = item.email;
-//           let phoneNumber = item.pnumber;
-//         }
-//       }
-    // })
+let amount = localStorage.getItem('cost')
 
+let id = RandomHexString(4);
+  let data = {
+    amount
+  }
 
 const payButton = document.getElementById("buttonPay");
 payButton.addEventListener("click", () => {
-  fetch("https://mor-start.onrender.com/listbooking", {
+  fetch("https://mor-start.onrender.com/payment", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body : JSON.stringify({
-      amount: localStorage.getItem('cost')
+      amount: localStorage.getItem('cost'),
+      idUser,
+      dateBooking: currentDate
     })
   })
-
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json(); // Chuyển đổi dữ liệu JSON
+    })
     .then((data) => {
-      console.log(data.payUrl);
       window.open(data.payUrl);
     })
     .catch((error) => {
       alert("Error:", error);
     });
 });
-
-
-
-

05ca2dd9cdcb678386706c3030d808fb45e59964
Hoàng <hoanghung20030@gmail.com>
2023-11-03 23:26:29 +0700

    lấy lại ban đầu

--- src/js/User/checkUser.js
+++ src/js/User/checkUser.js
@@ -2,7 +2,6 @@ let checkUser = localStorage.getItem("user_token_id");
 if (!checkUser) {
     console.log("chuaw  login");
 }else{
-    console.log("login roii");
     document.getElementById("loginBtn").style.display = "none";
     document.getElementById("registerBtn").style.display = "none";
     document.getElementById("menu_profile").style.display = "inline-block";
--- src/js/User/pay.js
+++ src/js/User/pay.js
@@ -1,65 +1,59 @@
-function RandomHexString(L) {
-  let hexstring = "";
-  for (let i = 0; i < L; i++) {
-    hexstring += Math.floor(Math.random() * 16).toString(16);
-  }
-  return hexstring;
-}
+// function RandomHexString(L) {
+//   let hexstring = "";
+//   for (let i = 0; i < L; i++) {
+//     hexstring += Math.floor(Math.random() * 16).toString(16);
+//   }
+//   return hexstring;
+// }
 
-  function getCurrentDate() {
-    var currentDate = new Date();
-    var year = currentDate.getFullYear();
-    var month = currentDate.getMonth() + 1; // Tháng bắt đầu từ 0
-    var day = currentDate.getDate();
-    // Định dạng lại ngày thành một chuỗi "YYYY-MM-DD"
-    var formattedDate = year + "-" + padZero(month) + "-" + padZero(day);
-    return formattedDate;
-  }
-  // Hàm này để đảm bảo rằng số tháng và ngày luôn có hai chữ số
-  function padZero(value) {
-    return value < 10 ? "0" + value : value;
-  }
-  // Gọi hàm để lấy ngày hiện tại
-  let currentDate = getCurrentDate();
-  let nameFilm = JSON.parse(localStorage.getItem('nameFilm'));
-  let dateWatch = JSON.parse(localStorage.getItem(''))
+//   function getCurrentDate() {
+//     var currentDate = new Date();
+//     var year = currentDate.getFullYear();
+//     var month = currentDate.getMonth() + 1; // Tháng bắt đầu từ 0
+//     var day = currentDate.getDate();
+//     // Định dạng lại ngày thành một chuỗi "YYYY-MM-DD"
+//     var formattedDate = year + "-" + padZero(month) + "-" + padZero(day);
+//     return formattedDate;
+//   }
+//   // Hàm này để đảm bảo rằng số tháng và ngày luôn có hai chữ số
+//   function padZero(value) {
+//     return value < 10 ? "0" + value : value;
+//   }
+//   // Gọi hàm để lấy ngày hiện tại
+//   let currentDate = getCurrentDate();
+//   let nameFilm = JSON.parse(localStorage.getItem('nameFilm'));
+//   let dateWatch = JSON.parse(localStorage.getItem(''))
 
-  let idUser = JSON.parse(localStorage.getItem('user_token_id'));
+//   let idUser = JSON.parse(localStorage.getItem('user_token_id'));
 
-  fetch('https://mor-start.onrender.com/user')
-    .then(res => res.json())
-    .then(data => {
-      for (const item of data) {     
-        if (item.id == idUser) {
-          nameUser = item.name;
-          let lastname = item.lastname;
-          let email = item.email;
-          let phoneNumber = item.pnumber;
-        }
-      }
-    })
+//   fetch('https://mor-start.onrender.com/user')
+//     .then(res => res.json())
+//     .then(data => {
+//       for (const item of data) {     
+//         if (item.id == idUser) {
+//           nameUser = item.name;
+//           let lastname = item.lastname;
+//           let email = item.email;
+//           let phoneNumber = item.pnumber;
+//         }
+//       }
+    // })
 
 
 const payButton = document.getElementById("buttonPay");
 payButton.addEventListener("click", () => {
-  fetch("https://mor-start.onrender.com/payment", {
+  fetch("https://mor-start.onrender.com/listbooking", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body : JSON.stringify({
-      idBooking: RandomHexString(4),
-      idUser : idUser,
-      filmName: localStorage.getItem('nameFilm'),
-      dateBooking: currentDate,
-      dateWatch: localStorage.getItem('dateWatch'),
-      time: localStorage.getItem('selectedHour'),
-      char: localStorage.getItem('Seat'),
       amount: localStorage.getItem('cost')
     })
   })
 
     .then((data) => {
+      console.log(data.payUrl);
       window.open(data.payUrl);
     })
     .catch((error) => {
